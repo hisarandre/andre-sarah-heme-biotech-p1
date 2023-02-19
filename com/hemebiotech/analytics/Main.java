aaf5cb36e47d31324574ad9c89018c7a4f92a1c5
Sarah Andre <q9tl3s653ks3-student@anonymous-codio.com>
2023-02-19 18:56:37 +0000

    main implementation

--- com/hemebiotech/analytics/Main.java
+++ com/hemebiotech/analytics/Main.java
@@ -4,13 +4,29 @@ import com.hemebiotech.analytics.ISymptomReader;
 import com.hemebiotech.analytics.ISymptomWriter;
 import com.hemebiotech.analytics.ReadSymptomDataFromFile;
 import com.hemebiotech.analytics.WriteSymptomDataToFile;
+import com.hemebiotech.analytics.AnalyticsCounter;
 
 import java.util.List;
 import java.util.Map;
 
 public class Main {
 
+  private static final String SYMPTOMSFILE = "symptoms.txt";
+  private static final String RESULTFILE = "result.out";
+
+
 	public static void main(String[] args) {
+
+    ISymptomReader reader = new ReadSymptomDataFromFile(SYMPTOMSFILE);
+    ISymptomWriter writer = new WriteSymptomDataToFile(RESULTFILE);
+    AnalyticsCounter counter = new AnalyticsCounter(reader, writer);
+
+    List<String> symptoms = counter.getSymptoms();
+    Map<String, Integer> countedSymptoms = counter.countSymptoms(symptoms);
+    Map<String, Integer> sortedSymptoms = counter.sortSymptoms(countedSymptoms);
+
+    counter.writeSymptoms(sortedSymptoms);
+
   }
   
 }
--- com/hemebiotech/analytics/WriteSymptomDataToFile.java
+++ com/hemebiotech/analytics/WriteSymptomDataToFile.java
@@ -8,6 +8,16 @@ import java.util.Map;
 
 public class WriteSymptomDataToFile implements ISymptomWriter {
 
+	private String filepath;
+	
+	/**
+	 * 
+	 * @param filepath a full or partial path to file with symptom strings in it, one per line
+	 */
+	public WriteSymptomDataToFile (String filepath) {
+		this.filepath = filepath;
+	}
+
   @Override
   public void writeSymptoms(Map<String, Integer> symptoms) {
   
